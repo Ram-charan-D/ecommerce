42b05f6d9406da00c18b61de712c49ab12a9028a
Ram-charan-D <aramcharan.d@gmail.com>
2026-09-20 18:49:48 +0530

    mongodb init

--- app.js
+++ app.js
@@ -1,129 +1,37 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
 
-const errorController = require("./controllers/error");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Import Prisma Client instance
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-// Middleware to attach user and their cart to request
-app.use(async (req, res, next) => {
-  try {
-    let user = await prisma.user.findUnique({
-      where: { id: 1 },
-      include: { cart: true },
-    });
-
-    // Seed dummy user & cart on first launch if not found
-    if (!user) {
-      user = await prisma.user.create({
-        data: {
-          name: "Max",
-          email: "test@test.com",
-          cart: { create: {} }, // Automatically creates attached cart
-        },
-        include: { cart: true },
-      });
-    }
-
-    req.user = user;
-    next();
-  } catch (err) {
-    console.error("User Middleware Error:", err);
-    next(err);
-  }
-});
-
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-// Start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
-// const path = require('path');
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const errorController = require('./controllers/error');
-// const sequelize = require('./util/database');
-// const Product = require('./models/product');
-// const User = require('./models/user');
-// const Cart = require('./models/cart');
-// const CartItem = require('./models/cart-item');
-// const Order = require('./models/order');
-// const OrderItem = require('./models/order-item');
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+});
 
 // app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
-// app.use(errorController.get404);
+app.use(errorController.get404);
 
-// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(Product);
-// User.hasOne(Cart);
-// Cart.belongsTo(User);
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-// Order.belongsTo(User);
-// User.hasMany(Order);
-// Order.belongsToMany(Product, { through: OrderItem });
-
-// sequelize
-//   // .sync({ force: true })
-//   .sync()
-//   .then(result => {
-//     return User.findById(1);
-//     // console.log(result);
-//   })
-//   .then(user => {
-//     if (!user) {
-//       return User.create({ name: 'Max', email: 'test@test.com' });
-//     }
-//     return user;
-//   })
-//   .then(user => {
-//     // console.log(user);
-//     return user.createCart();
-//   })
-//   .then(cart => {
-//     app.listen(3000);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
