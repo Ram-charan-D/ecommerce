fd81cdd4ab55d9ffe2f6c45cbee963cd0cc95069
Ram-charan-D <aramcharan.d@gmail.com>
2026-09-25 21:19:41 +0530

    user mongoose model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const adminRoutes = require("./routes/adminRoutes");
 const shopRoutes = require("./routes/shopRoutes");
 const errorController = require("./controllers/errorController");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 dns.setServers(["8.8.8.8", "1.1.1.1"]); // Google and Cloudflare DNS
@@ -17,13 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById("6ab54c08b9885d6676a597d6")
-  //   .then((user) => {
-  //     req.user = new User(user._id, user.name, user.email, user.cart);
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-  next();
+  User.findById("6ab697d44abdffd875ec23fc")
+    .then((user) => {
+      req.user = new User(user._id, user.name, user.email, user.cart);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -35,5 +34,13 @@ mongoose
   .connect(
     "mongodb+srv://ramachrand_db_user:m0iVv3iDVnuZRoCm@cluster0.ukvcdkp.mongodb.net/shop?appName=Cluster0",
   )
-  .then((res) => app.listen(3000))
+  .then((res) => {
+    const user = new User({
+      name: "Ramcharan",
+      email: "rcr@yopmail.com",
+      cart: { items: [] },
+    });
+    user.save();
+    app.listen(3000);
+  })
   .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,3 +1,25 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
 // const getDb = require("../util/database").getDb;
 // const { ObjectId } = require("mongodb");
 
